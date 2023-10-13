3a058553e98bb96fe9914dc5268d0e799d574315
munna tiysol <munna@tiysol.com>
2023-10-13 19:03:43 +0530

    photo required remove

--- models/authModel.js
+++ models/authModel.js
@@ -1,12 +1,31 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  mobile: { type: String, required: true }, // Change the data type to String
-  Type: { type: String, required: true },
-  photo: { type: String, required: true },
-  password: { type: String, required: true }
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  mobile: {
+    type: String,
+    required: true
+  }, // Change the data type to String
+  Type: {
+    type: String,
+    required: true
+  },
+  photo: {
+    type: String,
+    // required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('userdata', userSchema);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,7 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 const multer = require("multer");
 const path = require("path");
+const Auth = require("../middleware/auth")
 
 // Set storage for uploaded images using multer
 const storage = multer.diskStorage({
@@ -20,7 +21,7 @@ router.post('/signup', upload.fields([
   { name: 'photo', maxCount: 1 }
 ]), authController.signup);
 
-router.post('/login', authController.login);
+router.post('/login',Auth, authController.login);
 router.post('/update-password', authController.updatePassword);
 
 module.exports = router;
